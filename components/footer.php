2edcd6a0b5d84461109062235a3f5c97c236b96f
Kashish <132146409+kashiiishh@users.noreply.github.com>
2023-10-09 12:28:09 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +123-456-7890</a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i> +111-222-3333</a>
+         <a href="mailto:hotelsluxury@gmail.com"><i class="fas fa-envelope"></i> hotelsluxury@gmail.com</a>
+         
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Delhi, India - 700104</a>
+      </div>
+
+      <div class="box">
+         <a href="#home">Home</a>
+         <a href="#about">About</a>
+         <a href="bookings.php">My Bookings</a>
+         <a href="#reservation">Reservation</a>
+         <a href="#gallery">Gallery</a>
+         <a href="#contact">Contact</a>
+         <a href="#reviews">Reviews</a>
+      </div>
+
+      <div class="box">
+         <a href="#">Facebook <i class="fab fa-facebook-f"></i></a>
+         <a href="#">Twitter <i class="fab fa-twitter"></i></a>
+         <a href="#">Instagram <i class="fab fa-instagram"></i></a>
+         <a href="#">Linkedin <i class="fab fa-linkedin"></i></a>
+         
+      </div>
+
+   </div>
+
+   <div class="credit">&copy; Copyright @ 2022 | All Rights Reseved!</div>
+
+</section>
+
+<!-- footer section ends -->
---
+++ components/user_header.php
@@ -0,0 +1,24 @@
+<!-- header section starts  -->
+
+<section class="header">
+
+   <div class="flex">
+      <a href="#home" class="logo">HOTELS AND RESORTS</a>
+      <a href="admin/login.php" class="btn">Admin Login</a>
+      <div id="menu-btn" class="fas fa-bars"></div>
+   </div>
+
+   <nav class="navbar">
+      <a href="index.php#home">Home</a>
+      <a href="index.php#about">About</a>
+      <a href="index.php#reservation">Reservation</a>
+      <a href="index.php#gallery">Gallery</a>
+      <a href="index.php#contact">Contact</a>
+      <a href="index.php#reviews">Reviews</a>
+      <a href="bookings.php">My Bookings</a>
+      
+   </nav>
+
+</section>
+
+<!-- header section ends -->
